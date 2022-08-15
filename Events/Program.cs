15c82ca450d8f0d6b1aa77e1095e0169ce58d91b
Alexandra Ighodaro <alexandraighodaro@gmail.com>
2022-08-15 21:49:11 +0100

    Add project files.

---
+++ Events/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Events
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "video 1"};
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.Encode(video);
+        }
+    }
+    public class MailService //subscriber
+    {
+        public void OnVideoEncoded(object source, EventArgs args)
+        {
+            Console.WriteLine("MailService: Sending a mail...");
+        }
+    }
+}
---
+++ Events/VideoEncoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class VideoEncoder //publisher
+    {
+        
+        //Creating a delegate
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+        // creating an event based on above delegate
+        public event VideoEncodedEventHandler VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video before sending...");
+            Thread.Sleep(1000);
+            OnVideoEncoded();
+        }
+        //raise event
+        protected virtual void OnVideoEncoded()
+        {
+            // if the subscribers to publisher are not null
+            if (VideoEncoded != null)   
+                VideoEncoded(this, EventArgs.Empty);
+        }
+
+    }
+    public class Video
+    {
+        public string Title { get; set; }
+    }
+
+
+}
